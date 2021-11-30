68336ce3fcb3e3997d634e8925c0e68cb3f9f80a
Raphael Freitas <raphael.soarescivil@gmail.com>
2021-11-30 17:48:27 -0300

    primeira parte pronta

--- fundamentos/bloco-4-introducao-a-JavaScript-e-Logica-de-Programacao/exercicio4-3.js
+++ fundamentos/bloco-4-introducao-a-JavaScript-e-Logica-de-Programacao/exercicio4-3.js
@@ -1,6 +1,17 @@
-var capturando = "";
 
 function capturar() {
+    var capturando = "";
+    var quadrado = [];
     capturando = document.getElementById("valor").value;
-    document.getElementById("valorDigitado").innerHTML = capturando
+    for (let x = 0 ; x < capturando; x++){
+        for (var i = 0; i < capturando; i++){
+            quadrado.push("*");
+            if (i === capturando-1){
+                quadrado.push("<br>")
+            }
+        }
+    }
+    
+    quadrado = quadrado.join("")
+    document.getElementById("valorDigitado").innerHTML = quadrado
 }
